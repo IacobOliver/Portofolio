6f5033531a21f080b8a897939e024b57f1b0a054
Oliver <olialexander08@gmail.com>
2024-04-26 12:57:30 +0300

    fixed deploy2

--- src/app/(pages)/contact/page.tsx
+++ src/app/(pages)/contact/page.tsx
@@ -83,7 +83,7 @@ export default function Contact() {
       <div
         className="w-[25rem] mx-auto relative overflow-hidden  bg-gray-800 p-8 rounded-lg shadow-md before:w-24 before:h-24 before:absolute before:bg-purple-600 before:rounded-full before:-z-10 before:blur-2xl after:w-32 after:h-32 after:absolute after:bg-sky-400 after:rounded-full after:-z-10 after:blur-xl after:top-24 after:-right-12"
       >
-        <h2 className="text-3xl font-bold  gradientText mb-8">Let's get in touch!</h2>
+        <h2 className="text-3xl font-bold  gradientText mb-8">Let&apos;s get in touch!</h2>
 
         <div className="mb-4">
           <label className="block text-sm font-medium text-gray-300"
